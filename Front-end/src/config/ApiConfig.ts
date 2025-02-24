05f3f5d4a3e663a2ee3747fc6bd9394f0351dd42
ducmanh2503 <traubudz@gmail.com>
2025-02-24 18:08:27 +0700

    done film all

--- Front-end/src/config/ApiConfig.ts
+++ Front-end/src/config/ApiConfig.ts
@@ -6,10 +6,10 @@ const CREATE_FILM = `${BASE_URL}/movies`;
 const UPDATE_FILM = (id: number) => `${BASE_URL}/movies/${id}`;
 const DELETE_FILM = (id: number) => `${BASE_URL}/movies/${id}`;
 const FORCE_DELETE_FILM = (id: number) =>
-    `${BASE_URL}/movies/force-delete/${id}`;
+  `${BASE_URL}/movies/force-delete/${id}`;
 const RESTORE_FILM = (id: number) => `${BASE_URL}/movies/restore/${id}`;
 const DETAIL_DELETE_FILM = (id: number) =>
-    `${BASE_URL}/movies/show-movie-destroy/${id}`;
+  `${BASE_URL}/movies/show-movie-destroy/${id}`;
 
 const GET_DIRECTORS_LIST = `${BASE_URL}/directors`;
 const UPDATE_DIRECTORS = (id: number) => `${BASE_URL}/directors/${id}`;
@@ -23,21 +23,21 @@ const GET_GENRES = `${BASE_URL}/genres`;
 const DELETE_GENRES = (id: number) => `${BASE_URL}/genres/${id}`;
 
 export {
-    GET_FILM_LIST,
-    GET_FILM_DETAIL,
-    CREATE_FILM,
-    UPDATE_FILM,
-    DELETE_FILM,
-    FORCE_DELETE_FILM,
-    RESTORE_FILM,
-    DETAIL_DELETE_FILM,
-    GET_DIRECTORS_LIST,
-    UPDATE_DIRECTORS,
-    DELETE_DIRECTORS,
-    GET_ACTOR_LIST,
-    UPDATE_ACTOR,
-    DELETE_ACTOR,
-    GET_GENRES,
-    DELETE_GENRES,
-    URL_IMAGE,
+  GET_FILM_LIST,
+  GET_FILM_DETAIL,
+  CREATE_FILM,
+  UPDATE_FILM,
+  DELETE_FILM,
+  FORCE_DELETE_FILM,
+  RESTORE_FILM,
+  DETAIL_DELETE_FILM,
+  GET_DIRECTORS_LIST,
+  UPDATE_DIRECTORS,
+  DELETE_DIRECTORS,
+  GET_ACTOR_LIST,
+  UPDATE_ACTOR,
+  DELETE_ACTOR,
+  GET_GENRES,
+  DELETE_GENRES,
+  URL_IMAGE,
 };

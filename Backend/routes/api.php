9ab5bdc3ca471a456daf7636e772993979e5c3f4
Vandh <superman.98vn@gmail.com>
2025-02-27 21:30:51 +0700

    update logic showTime

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -25,53 +25,57 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->group(function () {
-    // Lấy thông tin user đã đăng nhập
-    Route::get('/user', function (Request $request) {
-        return response()->json($request->user());
-    });
-
-    // Movies
-    Route::apiResource('movies', MoviesController::class);
-    Route::delete('/movies/force-delete/{movie}', [MoviesController::class, 'forceDeleteSingle']);
-    Route::delete('/movies', [MoviesController::class, 'destroyMultiple']);
-    Route::delete('/movies/force-delete-multiple', [MoviesController::class, 'forceDeleteMultiple']);
-    Route::put('/movies/restore/{movie}', [MoviesController::class, 'restore']);
-    Route::get('/movies/show-movie-destroy/{movie}', [MoviesController::class, 'showMovieDestroy']);
-
-    // Room
-    Route::apiResource('room', RoomController::class);
-
-    // Seats
-    Route::post('/seats', [SeatController::class, 'store']);
-    Route::get('/seats/room/{room_id}', [SeatController::class, 'getSeats']);
-    Route::post('/seats/update-status', [SeatController::class, 'updateSeatStatus']);
-
-    // Showtimes
-    Route::apiResource('showTime', ShowTimeController::class);
-    Route::post('show-times/filter-by-date', [ShowTimeController::class, 'filterByDate']);
-    Route::post('show-times/filter-by-date-one', [ShowTimeController::class, 'filterByDateOne']);
-
-    // CalendarShow
-    Route::apiResource('calendarShow', CalendarShowController::class);
-
-    // Combo
-    Route::apiResource('combo', ComboController::class);
-    Route::delete('/combo', [ComboController::class, 'destroyMultiple']);
-    Route::delete('/combos/force-delete-multiple', [ComboController::class, 'forceDeleteMultiple']);
-    Route::delete('/combo/force/{combo}', [ComboController::class, 'forceDeleteSingle']);
-    Route::post('/combo/restore/{combo}', [ComboController::class, 'restore']);
-    Route::post('/combo/multiple/restore', [ComboController::class, 'restoreMultiple']);
-
-    // Thể loại phim, Diễn viên, Đạo diễn
-    Route::apiResource('genres', GenreController::class);
-    Route::apiResource('actors', ActorController::class);
-    Route::apiResource('directors', DirectorController::class);
-
-
-    // Đăng xuất
-    Route::post('/logout', [AuthController::class, 'logout']);
+// Route::middleware('auth:sanctum')->group(function () {
+// Lấy thông tin user đã đăng nhập
+Route::get('/user', function (Request $request) {
+    return response()->json($request->user());
 });
+
+// Movies
+Route::apiResource('movies', MoviesController::class);
+Route::delete('/movies/force-delete/{movie}', [MoviesController::class, 'forceDeleteSingle']);
+Route::delete('/movies', [MoviesController::class, 'destroyMultiple']);
+Route::delete('/movies/force-delete-multiple', [MoviesController::class, 'forceDeleteMultiple']);
+Route::put('/movies/restore/{movie}', [MoviesController::class, 'restore']);
+Route::get('/movies/show-movie-destroy/{movie}', [MoviesController::class, 'showMovieDestroy']);
+
+// Room
+Route::apiResource('room', RoomController::class);
+
+// Seats
+Route::post('/seats', [SeatController::class, 'store']);
+Route::get('/seats/room/{room_id}', [SeatController::class, 'getSeats']);
+Route::post('/seats/update-status', [SeatController::class, 'updateSeatStatus']);
+
+// Showtimes
+Route::apiResource('showTime', ShowTimeController::class);
+Route::post('show-times/in-range', [ShowTimeController::class, 'getShowTimesInDateRange']);//danh sách ngày
+Route::post('show-times/by-date', [ShowTimeController::class, 'getShowTimesByDate']);//lọc theo ngày cụ thể
+// lọc theo khoảng ngày
+Route::post('show-times/get-date-range-by-calendar', [ShowTimeController::class, 'getDateRangeByCalendarShow']);
+//xóa theo ngày cụ thể
+Route::delete('/showtimes/{id}/destroy-by-date/{selected_date}', [ShowTimeController::class, 'destroyByDate']);
+
+// CalendarShow
+Route::apiResource('calendarShow', CalendarShowController::class);
+
+// Combo
+Route::apiResource('combo', ComboController::class);
+Route::delete('/combo', [ComboController::class, 'destroyMultiple']);
+Route::delete('/combos/force-delete-multiple', [ComboController::class, 'forceDeleteMultiple']);
+Route::delete('/combo/force/{combo}', [ComboController::class, 'forceDeleteSingle']);
+Route::post('/combo/restore/{combo}', [ComboController::class, 'restore']);
+Route::post('/combo/multiple/restore', [ComboController::class, 'restoreMultiple']);
+
+// Thể loại phim, Diễn viên, Đạo diễn
+Route::apiResource('genres', GenreController::class);
+Route::apiResource('actors', ActorController::class);
+Route::apiResource('directors', DirectorController::class);
+
+
+// Đăng xuất
+Route::post('/logout', [AuthController::class, 'logout']);
+// });
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/resend-verification-email', [AuthController::class, 'resendVerificationEmail']);

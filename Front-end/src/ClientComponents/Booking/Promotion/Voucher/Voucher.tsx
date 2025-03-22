48f4b08c6e90eb04251bd34df078eaf12988917b
LAPTOP-VKKHL511\Admin <daominhkhanh2020@gmail.com>
2025-03-23 01:38:16 +0700

    commit cho anh Van

--- Front-end/src/ClientComponents/Booking/Promotion/Voucher/Voucher.tsx
+++ Front-end/src/ClientComponents/Booking/Promotion/Voucher/Voucher.tsx
@@ -11,90 +11,94 @@ import { useFinalPriceContext } from "../../../UseContext/FinalPriceContext";
 import CustomNotification from "../../Notification/Notification";
 
 const VoucherInfo = () => {
-  const { tokenUserId } = useAuthContext();
-  const {
-    setQuantityPromotion,
-    totalPricePoint,
-    setTotalPriceVoucher,
-    totalPriceVoucher,
-  } = usePromotionContextContext();
-  const { setTotalPrice, totalPrice } = useFinalPriceContext();
-  const { openNotification, contextHolder } = CustomNotification();
+    const { tokenUserId } = useAuthContext();
+    const {
+        setQuantityPromotion,
+        totalPricePoint,
+        setTotalPriceVoucher,
+        totalPriceVoucher,
+    } = usePromotionContextContext();
+    const { setTotalPrice, totalPrice } = useFinalPriceContext();
+    const { openNotification, contextHolder } = CustomNotification();
 
-  const [promoCode, setPromoCode] = useState<string>(""); // lấy dữ liệu mã khuyến mãi
-  const [isVoucherUsed, setIsVoucherUsed] = useState<boolean>(false); // ktra dùng hay chưa
+    const [promoCode, setPromoCode] = useState<string>(""); // lấy dữ liệu mã khuyến mãi
+    const [isVoucherUsed, setIsVoucherUsed] = useState<boolean>(false); // ktra dùng hay chưa
 
-  // set khuyến mãi
-  const onChangePromotion = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPromoCode(e.target.value);
-  };
+    // làm tạm thời
+    sessionStorage.setItem("promoCode", JSON.stringify(promoCode));
 
-  // Hàm xử lý khi thêm mã khuyến mãi
-  const handleAddPromotion = () => {
-    if (!promoCode || isVoucherUsed) {
-      openNotification({
-        description: "Mã chỉ có thể dùng 1 lần",
-      });
-      return;
-    } // Kiểm tra nếu mã đã dùng thì không gọi lại
-    getVoucher(promoCode);
-  };
+    // set khuyến mãi
+    const onChangePromotion = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setPromoCode(e.target.value);
+    };
 
-  // Gọi API kiểm tra mã khuyến mãi
-  const { mutate: getVoucher } = useMutation({
-    mutationFn: async (code: string) => {
-      const response = await axios.post(
-        GET_VOUCHER(code),
-        { name_code: promoCode },
-        { headers: { Authorization: `Bearer ${tokenUserId}` } }
-      );
-      return response.data; // Trả về dữ liệu từ API
-    },
-    onSuccess: (data) => {
-      const discountPercent = parseFloat(data.discount_percent);
+    // Hàm xử lý khi thêm mã khuyến mãi
+    const handleAddPromotion = () => {
+        if (!promoCode || isVoucherUsed) {
+            openNotification({
+                description: "Mã chỉ có thể dùng 1 lần",
+            });
+            return;
+        } // Kiểm tra nếu mã đã dùng thì không gọi lại
+        getVoucher(promoCode);
+    };
 
-      if (!isNaN(discountPercent)) {
-        // Tính giá sau khi áp dụng mã khuyến mãi
-        const newPrice =
-          (totalPrice + totalPricePoint) * (1 - discountPercent / 100);
-        setTotalPrice(newPrice - totalPricePoint);
-        setTotalPriceVoucher(newPrice);
-        setQuantityPromotion(1);
+    // Gọi API kiểm tra mã khuyến mãi
+    const { mutate: getVoucher } = useMutation({
+        mutationFn: async (code: string) => {
+            const response = await axios.post(
+                GET_VOUCHER(code),
+                { name_code: promoCode },
+                { headers: { Authorization: `Bearer ${tokenUserId}` } }
+            );
+            return response.data; // Trả về dữ liệu từ API
+        },
+        onSuccess: (data) => {
+            const discountPercent = parseFloat(data.discount_percent);
 
-        // Đánh dấu mã đã được sử dụng
-        setIsVoucherUsed(true);
-      }
-    },
+            if (!isNaN(discountPercent)) {
+                // Tính giá sau khi áp dụng mã khuyến mãi
+                const newPrice =
+                    (totalPrice + totalPricePoint) *
+                    (1 - discountPercent / 100);
+                setTotalPrice(newPrice - totalPricePoint);
+                setTotalPriceVoucher(newPrice);
+                setQuantityPromotion(1);
 
-    onError: () => {
-      openNotification({
-        description: "Mã không đúng hoặc không hợp lệ",
-      });
-    },
-  });
+                // Đánh dấu mã đã được sử dụng
+                setIsVoucherUsed(true);
+            }
+        },
 
-  return (
-    <div className={clsx(styles.promotionInput)}>
-      {contextHolder}
-      <h3 className={clsx(styles.title)}>Mã khuyến mãi</h3>
-      <Space.Compact>
-        <Input
-          value={promoCode}
-          onChange={onChangePromotion}
-          onPressEnter={handleAddPromotion}
-          placeholder="Nhập mã khuyến mãi"
-        />
-        <Button type="primary" onClick={handleAddPromotion}>
-          Thêm
-        </Button>
-      </Space.Compact>
-      {totalPriceVoucher !== 0 && (
-        <span className={clsx(styles.warning)}>
-          *ưu đãi được tính trước khi trừ điểm tích lũy(nếu có)
-        </span>
-      )}
-    </div>
-  );
+        onError: () => {
+            openNotification({
+                description: "Mã không đúng hoặc không hợp lệ",
+            });
+        },
+    });
+
+    return (
+        <div className={clsx(styles.promotionInput)}>
+            {contextHolder}
+            <h3 className={clsx(styles.title)}>Mã khuyến mãi</h3>
+            <Space.Compact>
+                <Input
+                    value={promoCode}
+                    onChange={onChangePromotion}
+                    onPressEnter={handleAddPromotion}
+                    placeholder="Nhập mã khuyến mãi"
+                />
+                <Button type="primary" onClick={handleAddPromotion}>
+                    Thêm
+                </Button>
+            </Space.Compact>
+            {totalPriceVoucher !== 0 && (
+                <span className={clsx(styles.warning)}>
+                    *ưu đãi được tính trước khi trừ điểm tích lũy(nếu có)
+                </span>
+            )}
+        </div>
+    );
 };
 
 export default VoucherInfo;

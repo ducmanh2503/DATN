bdcf94890ede4a3f80469f851bb78093e7484467
phancute <phanxjjnhtrai@gmail.com>
2025-03-28 22:54:47 +0700

    done commingfilm

--- Front-end/src/page/client/ComingFilm/ComingFilm.tsx
+++ Front-end/src/page/client/ComingFilm/ComingFilm.tsx
@@ -1,13 +1,37 @@
 import React, { useState } from "react";
 import ClientLayout from "../Layout";
 import ComingMain from "../../../ClientComponents/ComingMain/ComingMain";
+import FilterComingCinema from "../../../ClientComponents/FilterComingCinema/FilterComingCinema";
+import { useNavigate } from "react-router-dom";
 
 const ComingFilm = () => {
-    const [showChill, setShowChill] = useState(true);
+    const navigate = useNavigate();
+    const [filteredMovies, setFilteredMovies] = useState<any[]>([]);
+
+    const handleMovieClick = (movieId: number) => {
+        navigate(`/filmDetail/${movieId}`);
+    };
+
+    const handleFilterChange = (filterType: string, value: string | number) => {
+        console.log(`Filter thay đổi: ${filterType} = ${value}`);
+    };
+
+    const handleFilteredDataChange = (movies: any[]) => {
+        console.log(`Nhận được ${movies.length} phim sắp chiếu đã lọc`);
+        setFilteredMovies(movies);
+    };
+
     return (
-        <div>
+        <div className="coming-film-container">
             <ClientLayout>
-                <ComingMain showChill={true}></ComingMain>
+                <FilterComingCinema 
+                    onFilterChange={handleFilterChange}
+                    onFilteredDataChange={handleFilteredDataChange}
+                />
+                <ComingMain 
+                    showChill={handleMovieClick}
+                    filteredMovies={filteredMovies}
+                />
             </ClientLayout>
         </div>
     );

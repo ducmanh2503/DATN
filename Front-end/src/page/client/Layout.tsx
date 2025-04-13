a81a1387cabd6ba2c4ea4eb0ef6bcf6a3102e495
LAPTOP-VKKHL511\Admin <daominhkhanh2020@gmail.com>
2025-04-13 15:24:48 +0700

    public minigame

--- Front-end/src/page/client/Layout.tsx
+++ Front-end/src/page/client/Layout.tsx
@@ -1,22 +1,17 @@
-import React, { useEffect, useRef } from "react";
 import Header from "../../ClientComponents/Header/Header";
 import Navigate from "../../ClientComponents/Navigate/Navigate";
 import AppFooter from "../../ClientComponents/Footer/footer";
 import { FloatButton } from "antd";
-import { useLocation } from "react-router-dom";
-import { useStepsContext } from "../../ClientComponents/UseContext/StepsContext";
 const ClientLayout = ({ children }: any) => {
-  const location = useLocation();
-  const { setPathName } = useStepsContext();
-  return (
-    <div style={{ background: "var(--mainBase-color)" }}>
-      <Header></Header>
-      <Navigate></Navigate>
-      {children}
-      <FloatButton.BackTop />
-      <AppFooter />
-    </div>
-  );
+    return (
+        <div style={{ background: "var(--mainBase-color)" }}>
+            <Header></Header>
+            <Navigate></Navigate>
+            {children}
+            <FloatButton.BackTop />
+            <AppFooter />
+        </div>
+    );
 };
 
 export default ClientLayout;
